6d1acbd6e2afc4a62782bd7bbcab3ddcefd618ea
Radu - Eosif Mihailescu <radu.mihailescu@linux360.ro>
2012-07-31 23:58:38 +0100

    BUGFIX: Charge Pump generation, fixed again - Charge Pump cannot use interrupts because they're being disabled during acceleration/deceleration - Charge Pump can use set-and-forget CTC+OC mode, but in CTC mode the timer is reloaded from OCRxA only, even if OCRxB is used to toggle pin OCxB

--- timer.c
+++ timer.c
@@ -124,15 +124,6 @@ ISR(TIMER1_COMPA_vect) {
   SREG = sreg_save;
 }
 
-ISR(TIMER2_COMPA_vect) {
-  uint8_t sreg_save = SREG;
-  
-  TOGGLE(CHARGEPUMP_PIN);
-  
-  MEMORY_BARRIER();
-  SREG = sreg_save;
-}
-
 #endif /* ifdef HOST */
 
 /// initialise timer and enable system clock interrupt.
@@ -150,10 +141,11 @@ void timer_init()
 
   //And again, for the Charge Pump output
   // e.g. 16MHz / 8 / _80_ = 2 * 12.5kHz => 80 = 16MHz / 2 * 12.5 kHz * 8
-  OCR2A = F_CPU / (25000UL << 3); // Faster than multiplying by 8
-  TCCR2A = /*_BV(COM2B0) |*/ _BV(WGM21); // Toggle OC2B on compare
-  TCCR2B = _BV(CS21) ; // CTC mode and 1/8 prescaler
-  TIMSK2 = _BV(OCIE2A); // No interrupts
+  // OCR2B will drive the output but the timer gets reloaded from OCR2A only
+  OCR2B = OCR2A = F_CPU / (25000UL << 3); // Faster than multiplying by 8
+  TCCR2A = _BV(COM2B0) | _BV(WGM21); // Toggle OC2B on compare and CTC mode
+  TCCR2B = _BV(CS21) ; // 1/8 prescaler
+  TIMSK2 = 0; // No interrupts
 }
 
 #ifdef HOST
